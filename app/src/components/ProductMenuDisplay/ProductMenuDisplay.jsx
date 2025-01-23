023b1bfa284f6a2d831528949adbf72e06249a0e
jayAnjana13 <jayanjana13@gmail.com>
2025-01-23 22:26:34 +0530

    updated productMenuDisplay

--- app/src/components/ProductMenuDisplay/ProductMenuDisplay.jsx
+++ app/src/components/ProductMenuDisplay/ProductMenuDisplay.jsx
@@ -1,19 +1,50 @@
 import "./productMenu.css";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
-const ProductMenuDisplay = ({ id, name, price, description, image }) => {
+const ProductMenuDisplay = ({ id, name, price, description, image, badge }) => {
+  const navigate = useNavigate();
+  const { addToCart } = useContext(StoreContext);
+
+  const [isAdded, setIsAdded] = useState(false);
+
+  const handleCart = (e) => {
+    e.stopPropagation();
+    addToCart(id);
+    setIsAdded(true);
+  };
+
+  const MoveToSingleProduct = () => {
+    navigate("/single-product", { state: { id } });
+  };
+
   return (
     <div className="product-display">
-      <div className="product-item">
+      <div onClick={MoveToSingleProduct} className="product-item">
         <div className="product-item-img-conatiner">
           <img className="product-item-image" src={image} alt="" />
-          <div className="overlay">
-            <button className="add-to-cart-button">Add to Cart</button>
-            <div className="extra-options">
-              <span>Share</span>
-              <span>Compare</span>
-              <span>Like</span>
-            </div>
+          <div className={`overlay ${isAdded ? "disabled-overlay" : ""}`}>
+            <button
+              onClick={handleCart}
+              className="add-to-cart-button"
+              disabled={isAdded}
+            >
+              {isAdded ? "Added" : "Add to Cart"}
+            </button>
+            {!isAdded && (
+              <div className="extra-options">
+                <span>Share</span>
+                <span>Compare</span>
+                <span>Like</span>
+              </div>
+            )}
           </div>
+          {badge && (
+            <span className={`badge ${badge === "New" ? "New" : "discount"}`}>
+              {badge}
+            </span>
+          )}
         </div>
 
         <div className="product-item-info">
